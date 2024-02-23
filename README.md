67ff469b60408181b3fbd982536930f2e43b8b86
Julian B <bianquijulian@gmail.com>
2024-02-23 17:46:51 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# React Resume - Responsive and customizable CV / Resume
+# React + TypeScript + Vite
+You can easily create your fully beautiful CV / Resume by passing necessary props. <br/>
+Just clone or fork the repository and edit [data.js](https://github.com/bianqui149/react-resume/blob/master/src/Schemas/Data.js) inside schemas folder and build your own CV.
+
+- Light / Dark mode 🌗
+- Snowflakes mode ❄️
+
+[Preview](https://jbianqui.com)
+
+## Getting Started 👋
+### Built With 🛠️
+
+- [React](https://es.reactjs.org)
+- [React-helmet](https://github.com/nfl/react-helmet)
+
+### Installation 🔧
+```
+$ npm install
+> Edit Schemas/Data.js with your personal information
+```
+### Running the app 🏃
+```
+# dev mode
+$ yarn start or npm run dev
+```
---
+++ src/Schemas/Data.js
@@ -0,0 +1,116 @@
+export const Data = {
+  profile: {
+    name: "Julian",
+    ocupation: "FullStack Software Developer",
+    location: "Neuquen, Argentina",
+    email: "bianquijulian@gmail.com",
+    // telephone: "+56 971999705",
+    image: "images/profile.jpg",
+  },
+  aboutMe: {
+    label: "Profile",
+    description:
+      "I am a PHP developer with more than 5 years of experience, with a main focus on back-end.I have advanced PHP skills with several frameworks and cms. I'm also have experience with JavaScript framework for the front-end.\nFor the last 3 years, my main goal has been building sites and services with WordPress.\nAlso in the last few years I have been working with unit tests like PHPUnit and Cypress on both front-end and back-end.",
+  },
+  skills: {
+    technicalLabel: "Skills",
+    softLabel: "Skills",
+    technicalSkills: [
+      "Php",
+      "React",
+      "Python",
+      "Ruby",
+      "Java",
+      "Nodejs",
+      "Wordpress",
+      "Laravel",
+      "MYSQL",
+      "MONGO",
+      "Git",
+    ],
+    softLabel: "Other Skills",
+    softSkills: [
+      "Jenkins",
+      "Aws",
+      "Docker",
+      "Terminus",
+      "Comunicación",
+      "Proactividad",
+      "Creatividad",
+      "Autodidacta",
+      "Mentoria",
+    ],
+  },
+  socialMedia: {
+    label: "SOCIAL",
+    social: [
+      {
+        label: "jbianqui",
+        name: "linkedin",
+        url: "https://www.linkedin.com/in/julian-bianqui-ba861a78/",
+        className: "bxl-linkedin-square",
+      },
+      {
+        label: "jbianqui",
+        name: "github",
+        url: "https://github.com/bianqui149",
+        className: "bxl-github",
+      },
+      {
+        label: "Jbianqui",
+        name: "twiiter",
+        url: "https://www.instagram.com/julianbianqui/",
+        className: "bxl-instagram",
+      },
+    ],
+  },
+  experience: {
+    works: [
+      {
+        title: "FULL-STACK DEVELOPER",
+        period: "March 2023 - Actualidad",
+        company: "Split IO",
+        description: [
+          "Site reconstruction and brand rebranding.",
+          "Web application monitoring and security updates.",
+          "CI/CD pipeline with GITHUB."
+        ],
+      },
+      {
+        title: "FULL-STACK DEVELOPER",
+        period: "Oct. 2019 - Oct. 2021",
+        company: "Dinkuminteractive",
+        description: [
+          "Site reconstruction and brand rebranding.",
+          "Web application monitoring and security updates.",
+          "CI/CD pipeline with GITHUB."
+        ],
+      },
+    ],
+    academic: [
+      {
+        career: "University Technician in Web Application Development.",
+        date: "2016",
+        institution: "Universidad Nacional del Comahue",
+      },
+    ],
+    proyects: [
+      {
+        name: "Rionegro.com.ar",
+        company: "website",
+        period: "May. 2018 - Febraury. 2022",
+        description: [
+          "Design, development, security and deployment of the website of the digital newspaper",
+        ],
+      },
+      {
+        name: "Boston.com",
+        company: "website",
+        period: "Febraury. 2022 - Febraury. 2023",
+        description: [
+          "Control, development, security and deployment of APIs and website of the digital newspaper",
+        ],
+      },
+    ],
+  },
+};
